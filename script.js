65a4e18fd5cf92b1c843bb888e90486489ed69f8
p2101440 <zenkai0401@gmail.com>
2024-12-04 14:32:09 +0100

    Première version du code javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+// Fonction javascript qui affiche la chaine de caractères saisie dans un formulaire après avoir appuyé sur un bouton de validation
+/*
+function afficherTexte() {
+    var input = document.getElementById("new-todo-item-title").value;
+    alert(input);
+}
+
+document.getElementById("new-todo-item-add").addEventListener("click", afficherTexte);
+*/
+
+// Créez dynamiquement une section pour afficher les éléments validés
+var ul = document.createElement("ul");
+ul.id = "validated-items";
+document.body.appendChild(ul);
+
+function afficherTexte() {
+    var input = document.getElementById("new-todo-item-title").value;
+    if (input) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input));
+        ul.appendChild(li);
+        document.getElementById("new-todo-item-title").value = "";
+    }
+}
+
+document.getElementById("new-todo-item-add").addEventListener("click", afficherTexte);
