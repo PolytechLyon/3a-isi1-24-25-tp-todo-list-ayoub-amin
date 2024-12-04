40c545207ad706916f71f46569653c69b0094382
p2101440 <zenkai0401@gmail.com>
2024-12-04 15:29:01 +0100

    update

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-// Fonction javascript qui affiche la chaine de caractères saisie dans un formulaire après avoir appuyé sur un bouton de validation
-/*
-function afficherTexte() {
-    var input = document.getElementById("new-todo-item-title").value;
-    alert(input);
-}
-
-document.getElementById("new-todo-item-add").addEventListener("click", afficherTexte);
-*/
-
-// Créez dynamiquement une section pour afficher les éléments validés
-var ul = document.createElement("ul");
-ul.id = "validated-items";
-document.body.appendChild(ul);
+var ol = document.createElement("ol");
+ol.id = "validated-items";
+document.querySelector("ol").appendChild(ol);
+itemNumber = 1;
 
 function afficherTexte() {
     var input = document.getElementById("new-todo-item-title").value;
     if (input) {
         var li = document.createElement("li");
+
+        
         li.appendChild(document.createTextNode(input));
-        ul.appendChild(li);
+
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function() {
+            ol.removeChild(li);
+            itemNumber--;
+        });
+
+        var editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.addEventListener("click", function() {
+            var newText = prompt("Edit your item:", li.firstChild.textContent);
+            if (newText !== null) {
+                li.firstChild.textContent = newText;
+            }
+        });
+
+        let tabSpace = document.createElement('span');
+        tabSpace.style.marginLeft = '200px';  
+        li.appendChild(tabSpace);
+        li.appendChild(deleteButton);
+        let space = document.createTextNode(' ');
+        li.appendChild(space);
+        li.appendChild(editButton);
+
+        
+
+        ol.appendChild(li);
         document.getElementById("new-todo-item-title").value = "";
+        itemNumber++;
     }
 }
 
 document.getElementById("new-todo-item-add").addEventListener("click", afficherTexte);
-
-ul = document.createElement("ol");
-ul.id = "validated-items";
-document.body.appendChild(ul);
-
